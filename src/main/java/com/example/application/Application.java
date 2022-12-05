9ee7c07c4c53446866f5344306d617911ef4ca5c
sirGrishin <ilya.grishin31@gmail.com>
2022-12-05 16:33:36 +0200

    Initial commit

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,20 @@
+package com.example.application;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/application/views/list/MainLayout.java
@@ -0,0 +1,58 @@
+package com.example.application.views.list;
+
+import com.example.application.security.service.SecurityService;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.HighlightConditions;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+
+@Theme(themeFolder = "flowcrmtutorial")
+@PWA(
+        name = "PutinCRM",
+        shortName = "CRM",
+        offlinePath="offline.html",
+        offlineResources = { "./images/offline.png"}
+)
+public class MainLayout extends AppLayout {
+    private final SecurityService securityService;
+
+    public MainLayout(SecurityService securityService) {
+        this.securityService = securityService;
+        createHeader();
+        createDrawer();
+    }
+
+    private void createHeader() {
+        H1 logo = new H1("Putin CRM");
+        logo.addClassNames("text-l", "m-m");
+
+        Button logout = new Button("Log out", e -> securityService.logout());
+
+        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo, logout);
+
+        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.expand(logo);
+        header.setWidth("100%");
+        header.addClassNames("py-0", "px-m");
+
+        addToNavbar(header);
+
+    }
+
+    private void createDrawer() {
+        RouterLink listLink = new RouterLink("Список человеков", ListView.class);
+        listLink.setHighlightCondition(HighlightConditions.sameLocation());
+
+        addToDrawer(new VerticalLayout(
+                listLink,
+                new RouterLink("Графики", DashboardView.class)
+        ));
+    }
+}
